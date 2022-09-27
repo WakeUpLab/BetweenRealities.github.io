36de9288d4588a13d10fb5e589c10c3c8ed25a69
Jan <casasvil@protonmail.com>
2022-09-27 23:07:58 +0200

    el estilo parece correcto

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -35,10 +35,11 @@ function getUniqueValuesFromColumn() {
     
             
         });
-    
-        for(i in unique_col_values_dict) {
-            alert("Column index : " + i + " has Unique values : \n" + unique_col_values_dict[i]);
-        }
+        
+        // omitido
+        // for(i in unique_col_values_dict) {
+        //     alert("Column index : " + i + " has Unique values : \n" + unique_col_values_dict[i]);
+        // }
     
         updateSelectOptions(unique_col_values_dict)
     
